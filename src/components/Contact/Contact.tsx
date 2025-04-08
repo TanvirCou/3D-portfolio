e454c292d817b2394043f2beae18b0249423dabc
Tanvir Ahmed <ahmed.tnvr999@gmail.com>
2025-04-09 03:28:43 +0600

    Fix: contact section's bug fixed.

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -178,7 +178,8 @@ const Contact = () => {
               className="w-full rounded-sm border border-white bg-transparent px-1.5"
             />
             <motion.button
-              whileHover={{ scaleY: 1.1 }}
+              initial={{ scale: 1 }}
+              whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
               transition={{ duration: 0.5 }}
               type="submit"
